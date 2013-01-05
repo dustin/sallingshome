80e049d9db56593f1161e5cef3ffc26fefb8fba1
Dustin Sallings <dustin@spy.net>
2013-01-05 01:23:26 -0800

    Filling out the data model a bit.

--- sallingshome/data.go
+++ sallingshome/data.go
@@ -2,11 +2,23 @@ package sallingshome
 
 import (
 	"time"
+
+	"appengine/datastore"
 )
 
 type Task struct {
-	Name   string    `json:"name"`
-	Period int       `json:"period"`
-	Value  int       `json:"value"` // cents
-	Next   time.Time `json:next"`
+	Name     string    `json:"name"`
+	Period   int       `json:"period"`
+	RType    string    `json:"repeatType"`
+	Assignee string    `json:"assignee"`
+	Value    int       `json:"value"` // cents
+	Next     time.Time `json:next"`
+	Disabled bool      `json:"disabled"`
+}
+
+type LoggedTask struct {
+	Task      *datastore.Key
+	User      *datastore.Key
+	Completed time.Time
+	Paid      *time.Time
 }
